9f48f6877121976755ef2ecc7f8152c12a0c18e2
Abel Kalum <chumbabel@gmail.com>
2020-01-28 20:09:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,10 @@
 class Dog
-  def name=(dog_name)
-    this_dogs_name = dog_name
+ 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
   end
  
   def name
-    this_dogs_name
+    @this_dogs_name
   end
-  
-  def bark
-    this_dogs_bark
-  end
-  
 end
-
-fido = Dog.new
-fido.name = "Fido"
- 
-fido.name
